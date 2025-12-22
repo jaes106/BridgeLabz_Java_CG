8061156be2638dfe1411e1b235eee2c55e00b36b
Jason Abraham Rajan <jasonrajan20@gmail.com>
2025-12-22 12:31:16 +0530

    [Jason] : added ObjectOrientedProgramming

--- src/CoreProgramming/ProgrammingElements/level1/KilometersToMiles.java
+++
@@ -1,9 +0,0 @@
-package CoreProgramming.ProgrammingElements.level1;
-
-public class KilometersToMiles {
-    public static void main(String[] args) {
-        double kiloMeters = 10.8;
-        double miles = kiloMeters * 1.6;
-        System.out.printf("The distance 10.8km in miles is %.2f ", miles);
-    }
-}
